d27deadc473d6e4dd55a6365e6a25e18b82cb18f
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-18 20:15:44 -0300

    Add project files.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,27 @@
+﻿float n1;
+float n2;
+
+Console.WriteLine("Digite o primeiro numero: ");
+n1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo numero: ");
+n2 = float.Parse(Console.ReadLine());
+
+float soma = n1 + n2;
+float subtracao = n1 - n2;
+float multiplicacao = n1 * n2;
+if (n2 == 0) // && and, || or
+{
+    Console.WriteLine("Impossivel dividir por 0");
+}
+else
+{
+    float divisao = n1 / n2;
+    Console.WriteLine($"Soma é igual: {soma}\nSubtração é igual: {subtracao}\nMultiplicação é igual {multiplicacao}\nDivisão é igual: {divisao}");
+
+}
+
+
+
+
+
+
