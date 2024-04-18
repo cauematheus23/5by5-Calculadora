bfdf7593e0ab8e5293cfef7dbdba3c2b7066840e
cauematheus23 <cauematheus_23@hotmail.com>
2024-04-18 20:17:19 -0300

    updates commit

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,27 +1,57 @@
-﻿float n1;
-float n2;
-
-Console.WriteLine("Digite o primeiro numero: ");
+﻿float n1, n2, sum, sub, mul, div;
+int c;
+Console.Write("Digite o primeiro numero: ");
 n1 = float.Parse(Console.ReadLine());
-Console.WriteLine("Digite o segundo numero: ");
+Console.Write("Digite o segundo numero: ");
 n2 = float.Parse(Console.ReadLine());
-
-float soma = n1 + n2;
-float subtracao = n1 - n2;
-float multiplicacao = n1 * n2;
-if (n2 == 0) // && and, || or
+c = 1;
+while (c != 0)
 {
-    Console.WriteLine("Impossivel dividir por 0");
+    Console.WriteLine("\nEscolha  uma opção\n \n[1]Soma\n[2]Subtração\n[3]Multiplicação\n[4]Divisao\n[5]Escolher novos numeros\n[0]Sair");
+    c = int.Parse(Console.ReadLine());
+    if (c == 1)
+    {
+        sum = n1 + n2;
+        Console.WriteLine($"{n1} + {n2} = {sum}");
+    }
+    else if (c == 2)
+    {
+        sub = n1 - n2;
+        Console.WriteLine($"{n1} - {n2} = {sub}");
+    }
+    else if (c == 3)
+    {
+        mul = n1 * n2;
+        Console.WriteLine($"{n1} * {n2} = {mul}");
+    }
+    else if (c == 4)
+    {
+        if (n2 == 0) // && and, || or
+        {
+            do
+            {
+                Console.WriteLine("Impossivel dividir por 0");
+                Console.WriteLine("Digite um  novo valor para n2");
+                n2 = float.Parse(Console.ReadLine());
+            } while (n2 == 0);
+
+        }
+        div = n1 / n2;
+        Console.WriteLine($"{n1} / {n2} = {div}");
+    }
+    else if (c == 0)
+    {
+        break;
+    }
+    else if (c == 5)
+    {
+        Console.WriteLine("Digite o primeiro numero: ");
+        n1 = float.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo numero: ");
+        n2 = float.Parse(Console.ReadLine());
+    }
+    else
+    {
+        Console.WriteLine("Valor invalido, digite um  valor entre 0 e 5");
+    }
 }
-else
-{
-    float divisao = n1 / n2;
-    Console.WriteLine($"Soma é igual: {soma}\nSubtração é igual: {subtracao}\nMultiplicação é igual {multiplicacao}\nDivisão é igual: {divisao}");
-
-}
-
-
-
-
-
-
